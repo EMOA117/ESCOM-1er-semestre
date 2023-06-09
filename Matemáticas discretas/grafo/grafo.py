00b76a45b6ed0f39cdabadf17f2aeed79a59ca35
EMOA117 <emontesdeocaa1900@alumno.ipn.mx>
2023-06-08 22:44:40 -0600

    Validación programa de grafos

--- Matemáticas discretas/grafo/grafo.py
+++ Matemáticas discretas/grafo/grafo.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 def unir_nodos(graph, n, m):
     nodes = list(graph.nodes)
 
+    # if n == 2 and m==0:
+    #     graph.add_edge(0,0)
+    #     graph.add_edge(1,1)
+
     for i in range(len(nodes)):
         x = nodes[i]
 
@@ -12,7 +16,7 @@ def unir_nodos(graph, n, m):
 
             if (int(x) + int(y)) % n == m:
                 graph.add_edge(x, y)
-
+                print(x,"R" ,y)
     return graph
 
 def generar_grafo(n, m):
@@ -26,10 +30,11 @@ def generar_grafo(n, m):
     # Unir los nodos que cumplen la condición en el grafo
     grafo_resultante = unir_nodos(grafo, n, m)
 
-    # Dibujar el grafo
-    nx.draw_circular(grafo_resultante, with_labels=True, node_color='cyan', node_size=200, font_size=10, edge_color='blue')
-
-    # print("nodos:", grafo.order, "  conexiones:",grafo.size)
+    if n == 2 and m==0:
+        nx.draw_random(grafo_resultante, with_labels=True, node_color='cyan', node_size=500, font_size=10, edge_color='blue')
+    else:
+        # Dibujar el grafo
+        nx.draw_circular(grafo_resultante, with_labels=True, node_color='cyan', node_size=500, font_size=10, edge_color='blue')
     
     # Mostrar el grafo
     plt.show()
@@ -38,5 +43,11 @@ def generar_grafo(n, m):
 n = int(input("Ingrese el valor de n: "))
 m = int(input("Ingrese el valor de m: "))
 
+if n <= 0:
+    print("n no puede ser negativo o cero")
+elif m >= n:
+    print("m no puede ser mayor que n")
+elif n>=0:
+    generar_grafo(n, m)
 # Generar y visualizar el grafo
-generar_grafo(n, m)
+# generar_grafo(n, m)
